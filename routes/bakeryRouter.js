9103a238fa28bdea44c60d7033747625a1abb45f
Amanda Abrams <aca62997@gmail.com>
2022-03-18 12:56:20 -0400

    Express REST API with MongoDB and Mongoose Part 2

--- routes/bakeryRouter.js
+++ routes/bakeryRouter.js
@@ -72,4 +72,145 @@ bakeryRouter.route('/:bakeryId')
     .catch(err => next(err));
 });
 
+bakeryRouter.route('/:bakeryId/comments')
+.get((req, res, next) => {
+    Bakery.findById(req.params.bakeryId)
+    .then(bakery => {
+        if (bakery) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(bakery.comments);
+        } else {
+            err = new Error(`Bakery ${req.params.bakeryId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.post((req, res, next) => {
+    Bakery.findById(req.params.bakeryId)
+    .then(bakery => {
+        if (bakery) {
+            bakery.comments.push(req.body);
+            bakery.save()
+            .then(bakery => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(bakery);
+            })
+            .catch(err => next(err));
+        } else {
+            err = new Error(`Bakery ${req.params.bakeryId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /bakeries/${req.params.bakeryId}/comments`);
+})
+.delete((req, res, next) => {
+    Bakery.findById(req.params.bakeryId)
+    .then(bakery => {
+        if (bakery) {
+            for (let i = (bakery.comments.length-1); i >= 0; i--) {
+                bakery.comments.id(bakery.comments[i]._id).remove();
+            }
+            bakery.save()
+            .then(bakery => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(bakery);
+            })
+            .catch(err => next(err));
+        } else {
+            err = new Error(`Bakery ${req.params.bakeryId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+});
+
+bakeryRouter.route('/:bakeryId/comments/:commentId')
+.get((req, res, next) => {
+    Bakery.findById(req.params.bakeryId)
+    .then(bakery => {
+        if (bakery && bakery.comments.id(req.params.commentId)) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(bakery.comments.id(req.params.commentId));
+        } else if (!bakery) {
+            err = new Error(`Bakery ${req.params.bakeryId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Comment ${req.params.commentId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /bakeries/${req.params.bakeryId}/comments/${req.params.commentId}`);
+})
+.put((req, res, next) => {
+    Bakery.findById(req.params.bakeryId)
+    .then(bakery => {
+        if (bakery && bakery.comments.id(req.params.commentId)) {
+            if (req.body.rating) {
+                bakery.comments.id(req.params.commentId).rating = req.body.rating;
+            }
+            if (req.body.text) {
+                bakery.comments.id(req.params.commentId).text = req.body.text;
+            }
+            bakery.save()
+            .then(bakery => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(bakery);
+            })
+            .catch(err => next(err));
+        } else if (!bakery) {
+            err = new Error(`Bakery ${req.params.bakeryId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Comment ${req.params.commentId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.delete((req, res, next) => {
+    Bakery.findById(req.params.bakeryId)
+    .then(bakery => {
+        if (bakery && bakery.comments.id(req.params.commentId)) {
+            bakery.comments.id(req.params.commentId).remove();
+            bakery.save()
+            .then(bakery => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(bakery);
+            })
+            .catch(err => next(err));
+        } else if (!bakery) {
+            err = new Error(`Bakery ${req.params.bakeryId} not found`);
+            err.status = 404;
+            return next(err);
+        } else {
+            err = new Error(`Comment ${req.params.commentId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+});
+
 module.exports = bakeryRouter;
