afb9da49d182fe84e40c91eb2508978a93cc0fa6
Amanda Abrams <aca62997@gmail.com>
2022-03-18 12:40:12 -0400

    Express REST API with MongoDB and Mongoose Part 1

--- routes/bakeryRouter.js
+++ routes/bakeryRouter.js
@@ -1,45 +1,75 @@
 const express = require('express');
+const Bakery = require('../models/bakery');
+
 const bakeryRouter = express.Router();
 
 bakeryRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
+.get((req, res, next) => {
+    Bakery.find()
+    .then(bakeries => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(bakeries);
+    })
+    .catch(err => next(err));
 })
-.get((req, res) => {
-    res.end('Will send bakery info to you');
-})
-.post((req, res) => {
-    res.end(`Will add the bakery: ${req.body.name} with description: ${req.body.description}`);
+.post((req, res, next) => {
+    Bakery.create(req.body)
+    .then(bakery => {
+        console.log('Bakery Created ', bakery);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(bakery);
+    })
+    .catch(err => next(err));
 })
 .put((req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /bakeries');
 })
-.delete((req, res) => {
-    res.end('Deleting all bakeries');
+.delete((req, res, next) => {
+    Bakery.deleteMany()
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
 });
 
-bakeryRouter.route("/:bakeryId")
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req, res) => {
-    res.end(`Will send details of the bakery: ${req.params.bakeryId} to you`);
+bakeryRouter.route('/:bakeryId')
+.get((req, res, next) => {
+    Bakery.findById(req.params.bakeryId)
+    .then(bakeryRouter => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(bakery);
+    })
+    .catch(err => next(err));
 })
 .post((req, res) => {
     res.statusCode = 403;
     res.end(`POST operation not supported on /bakeries/${req.params.bakeryId}`);
 })
-.put((req, res) => {
-    res.write(`Updating the bakery: ${req.params.bakeryId}\n`);
-    res.end(`Will update the bakery: ${req.body.name} with description: ${req.body.description}`);
+.put((req, res, next) => {
+    Bakery.findByIdAndUpdate(req.params.bakeryId, {
+        $set: req.body
+    }, { new: true })
+    .then(bakery => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(bakery);
+    })
+    .catch(err => next(err));
 })
-.delete((req, res) => {
-    res.end(`Deleting bakery: ${req.params.bakeryId}`);
+.delete((req, res, next) => {
+    Bakery.findByIdAndDelete(req.params.bakeryId)
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
 });
 
 module.exports = bakeryRouter;
